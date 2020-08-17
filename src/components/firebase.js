3b58022f91499a9b6f97109d2b9de1022cbd312d
rohit10000 <rohitsingh18997@gmail.com>
2020-08-17 22:19:09 +0530

    setup react app

---
+++ src/components/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBr95GBb1CN0hUz1pnn-ZMGYT5Rp1KpgxQ",
+    authDomain: "tic-toc-clone-a5781.firebaseapp.com",
+    databaseURL: "https://tic-toc-clone-a5781.firebaseio.com",
+    projectId: "tic-toc-clone-a5781",
+    storageBucket: "tic-toc-clone-a5781.appspot.com",
+    messagingSenderId: "1008247960990",
+    appId: "1:1008247960990:web:748d2c03538d136ab5528f",
+    measurementId: "G-T32YVHB3RZ"
+};
